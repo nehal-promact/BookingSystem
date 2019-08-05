fc2ebd936c584603b2db35984fd52f6eb8f7adb3
Nehal <nehal@promactinfo.com>
2019-08-05 11:24:56 +0530

    booking create and dialogbox component

---
+++ frontend/src/app/bookings/booking-create/booking-create.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BookingCreateComponent } from './booking-create.component';
+
+
+
+@NgModule({
+  declarations: [BookingCreateComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class BookingCreateModule { }
