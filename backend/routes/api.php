fb9e242d6fb64809cb89175fa797d745e3811b7c
Priyadarshini <priyadarshini@promactinfo.com>
2019-08-20 14:39:42 +0530

    routes file changes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -15,24 +15,14 @@ use Illuminate\Http\Request;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-    Route::apiResource('booking', 'API\BookingController');
-	Route::apiResource('space', 'API\SpacesController')->except([
-    	'create', 'edit'
-	]);	
+   return $request->user();
+   Route::apiResource('booking', 'API\BookingController');
+    Route::apiResource('space', 'API\SpacesController')->except([
+       'create', 'edit'
+    ]);    
 });
-
-<<<<<<< HEAD
-Route::apiResource('space', 'API\SpacesController')->except([
-    'create', 'edit'
-]);
-Route::apiResource('user', 'API\UserController');
-Route::apiResource('booking', 'API\BookingController');
-=======
-Route::post('login', 'API\UserController@login');	
+Route::post('login', 'API\UserController@login');    
 Route::apiResource('user', 'API\UserController');
 
-
->>>>>>> e47a4ec2cec7dd8415d68b6108b304200ec51b25
 //Route::get('getSpaces','API\SpacesController@index');
 //Route::post('addSpace', 'API\SpacesController@create');
