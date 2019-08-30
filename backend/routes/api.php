c040c6c4a23ffca926398c2fe90452ffe3eecc0c
Nehal <nehal@promactinfo.com>
2019-08-30 13:49:05 +0530

    backend- booking and api file merge issue

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -18,6 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
     Route::apiResource('booking', 'API\BookingController');
 });
+Route::apiResource('booking', 'API\BookingController');
 Route::get('getBookingsForDayView','API\BookingController@getBookingsForDayView');
 Route::get('getBookingsForMonthView','API\BookingController@getBookingsForMonthView');
 Route::apiResource('user', 'API\UserController');
